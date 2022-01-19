5e3c0169316300b3a0bb9ad372c4878c405de69e
AdrianoCassano <cassano.adriano@gmail.com>
2022-01-18 22:46:45 -0300

    Primer push

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require ("express")
+
+const app = express()
+
+app.use ( express.static("public"))
+
+app.get("/home", (req,res) => {
+    res.sendFile(__dirname + "/views/home.html")
+})
+
+app.get("/register", (req,res) => {
+    res.sendFile(__dirname + "/views/register.html")
+})
+
+app.get("/login", (req,res) => {
+    res.sendFile(__dirname + "/views/login.html")
+})
+
+app.listen (3000, () =>{
+    console.log("server up")
+})
